421a5769e9b20a14dc8bca2dbffb20b750c51cc3
Андрей Марчук <marchuk-8282@mail.ru>
2023-07-09 23:32:10 +0200

    Д.З 19.1

---
+++ HW_19.1/main.py
@@ -0,0 +1,60 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_index(self):
+        with open('main_page.html') as f:
+            return f.read()
+
+    def __get_article_content(self, page_address):
+        if page_address == 'Главная':
+            with open('main_page.html') as f:
+                return f.read()
+        if page_address == 'Каталог':
+            with open('Catalog.html') as f:
+                return f.read()
+        elif page_address == 'Категории':
+            with open('category.html') as f:
+                return f.read()
+        elif page_address == 'Контакты':
+            with open('contacts.html') as f:
+                return f.read()
+        return 'Article not found!'
+
+    def __get_blog_article(self, page_address):
+        return f"""
+        <html><head><title>Home_work_19.1</title></head><body>
+        <p>{self.__get_article_content(page_address)}</p>
+        </body>
+        </html>
+        """
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        page_address = query_components.get('page')
+        page_content = self.__get_index()
+        if page_address:
+            page_content = self.__get_blog_article(page_address[0])
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
